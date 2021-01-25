f63a651a93504fed597852c5096c7ceb09ec5991
heidibh <heidi.bergh-hoff@unit.no>
2021-01-25 17:57:02 +0100

    Dev

--- init-clone-repos.sh
+++ init-clone-repos.sh
@@ -4,22 +4,22 @@ mkdir /c/antora
 cd /c/antora
 
 
-mkdir /c/antora/modelx-no
-cd /c/antora/modelx-no
+mkdir /c/antora/unit-norge
+cd /c/antora/unit-norge
 
 echo ""
 echo ""
 echo "Cloning playbook..."
 echo "==================="
-git clone https://github.com/modelx-no/modelx-no-playbook
+git clone https://github.com/unit-norge/antora-playbook
 
 
 echo ""
 echo ""
-echo "Cloning antora-ui (temporary solution)"
-echo "======================================"
-git clone https://gitlab.com/digdir/antora-ui
-#git clone https://github.com/modelx-no/antora-ui
+#echo "Cloning antora-ui (temporary solution)"
+#echo "======================================"
+#git clone https://gitlab.com/digdir/antora-ui
+#git clone https://github.com/unit-norge/antora-ui
 
 
 
@@ -27,18 +27,18 @@ echo ""
 echo ""
 echo "Cloning components and modules..."
 echo "================================="
-mkdir /c/antora/modelx-no/components
-mkdir /c/antora/modelx-no/components/unit-ra
-cd /c/antora/modelx-no/components/unit-ra
+mkdir /c/antora/unit-norge/components
+mkdir /c/antora/unit-norge/components/unit-ra
+cd /c/antora/unit-norge/components/unit-ra
 
 
 echo ""
 echo ""
 echo "Cloning output repos"
 echo "===================="
-mkdir /c/antora/modelx-no/output/
-mkdir /c/antora/modelx-no/output/github-pages/
-cd /c/antora/modelx-no/output/github-pages/
-git clone https://github.com/modelx-no/modelx-no.github.io
+mkdir /c/antora/unit-norge/output/
+mkdir /c/antora/unit-norge/output/
+cd /c/antora/unit-norge/output/
+git clone https://github.com/unit-norge/unit-norge.github.io
 echo ""
 echo "Done"
--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #git config core.autocrlf true
 echo "Updating component unit-ra..."
 echo "=========================="
-cd /c/antora/modelx-no/components/unit-ra
+cd /c/antora/unit-norge/components/unit-ra
 git checkout main
 git add .
 git commit -m "Dev"
@@ -11,7 +11,7 @@ git push origin main
 echo ""
 echo "Updating playbook..."
 echo "===================="
-cd /c/antora/modelx-no/modelx-no-playbook
+cd /c/antora/unit-norge/antora-playbook
 git checkout main
 git add .
 git commit -m "Dev"
@@ -24,7 +24,7 @@ antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_edit
 echo ""
 echo "Updating site..."
 echo "================"
-cd /c/antora/modelx-no/output/github-pages/modelx-no.github.io
+cd /c/antora/unit-norge/output/unit-norge.github.io
 #Make it work with github:
 touch .nojekyll
 #
